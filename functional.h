99cd94f78dfac346411bab25bc97360ffbbfa717
sdziuda <szymondziuda95@gmail.com>
2023-01-22 19:31:44 +0100

    Added functional elements

--- functional.h
+++ functional.h
@@ -1,4 +1,28 @@
 #ifndef FUNCTIONAL_H
-#define JNP1_IMAGES_FUNCTIONAL_H
+#define FUNCTIONAL_H
 
-#endif FUNCTIONAL_H
+template<typename F, typename... Fs>
+auto compose(F f, Fs... fs) {
+    return [=](auto x) { return compose(fs...)(f(x)); };
+}
+
+template <typename F>
+auto compose(F f) {
+    return f;
+}
+
+auto compose() {
+    return [](auto x) { return x; };
+}
+
+template <typename H, typename... Fs>
+auto lift(H h, Fs... fs) {
+    return [=](auto x) { return h(fs(x)...); };
+}
+
+template <typename F>
+auto lift(F f) {
+    return f;
+}
+
+#endif
